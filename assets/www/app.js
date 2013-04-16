36d5385472a9a2440a6e10fd4cc314cb7ef0a07f
yrkapil <yrkapil@gmail.com>
2013-04-16 12:30:33 +0530

    Added comments to app.js

--- assets/www/app.js
+++ assets/www/app.js
@@ -1,9 +1,11 @@
 Ext.application({
+	//Navigator App Name Space
 	name:"Navigator",
 	models:["SearchListModel","SearchListDetailsModel","Steps"],
 	views:["Main","Dashboard","Search","SearchList","SearchListDetails"],
 	controllers:["DashboardController","SearchController","SearchListController","SearchListDetailsController"],
 	stores:["SearchListStore","SearchListDetailsStore"],
+	//Global Variables
 	baseGoogleGeocodeAPIUrl:'http://maps.googleapis.com/maps/api/geocode/',
 	baseGooglePlacesAPIUrl:'https://maps.googleapis.com/maps/api/place/nearbysearch/',
 	baseGoogleDirectionsAPIUrl:'http://maps.googleapis.com/maps/api/directions/',
